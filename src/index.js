f53921575da162f0bce5451679acbc7c1cb70924
hendridg <hendridg@gmail.com>
2022-02-02 13:37:37 -0400

    Add nav functionality to deploy type food selected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import './style.css';
 import getAllData from './getDataFood.js';
 
+// eslint-disable-next-line operator-linebreak
+const [liBeefElement, liPastaElement, liSeafoodElement] =
+  document.querySelectorAll('li');
+let typeFoodSelected = liBeefElement;
 // links to get data from food api
 const allBeefURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
-// const allPastaURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=pasta';
-// const allSeafoodURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+const allPastaURL = `
+  https://www.themealdb.com/api/json/v1/1/filter.php?c=pasta`;
+const allSeafoodURL = `
+  https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`;
 
 // // link to likes and message store api
 
@@ -13,4 +19,37 @@ const allBeefURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
 // const postMessageLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/cq8n4ydnTGepOHztUKmY/comments';
 // const getMessagesLink = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/cq8n4ydnTGepOHztUKmY/comments?item_id='; // you need to add item id at end of url
 
-getAllData(allBeefURL);
+liBeefElement.addEventListener('click', () => {
+  typeFoodSelected = liBeefElement;
+  liBeefElement.classList.add('selected');
+  liPastaElement.classList.remove('selected');
+  liSeafoodElement.classList.remove('selected');
+  typeFoodSelected.textContent = 'Beef';
+  liPastaElement.textContent = 'Pasta';
+  liSeafoodElement.textContent = 'Seafood';
+  getAllData(allBeefURL, typeFoodSelected);
+});
+
+liPastaElement.addEventListener('click', () => {
+  typeFoodSelected = liPastaElement;
+  liPastaElement.classList.add('selected');
+  liBeefElement.classList.remove('selected');
+  liSeafoodElement.classList.remove('selected');
+  typeFoodSelected.textContent = 'Pasta';
+  liBeefElement.textContent = 'Beef';
+  liSeafoodElement.textContent = 'Seafood';
+  getAllData(allPastaURL, typeFoodSelected);
+});
+
+liSeafoodElement.addEventListener('click', () => {
+  typeFoodSelected = liSeafoodElement;
+  liSeafoodElement.classList.add('selected');
+  liBeefElement.classList.remove('selected');
+  liPastaElement.classList.remove('selected');
+  typeFoodSelected.textContent = 'Seafood';
+  liBeefElement.textContent = 'Beef';
+  liPastaElement.textContent = 'Past';
+  getAllData(allSeafoodURL, typeFoodSelected);
+});
+
+getAllData(allBeefURL, typeFoodSelected);
