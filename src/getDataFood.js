e9f6e69aaf2c22989a724d81da9b1d3c46efc10a
hendridg <hendridg@gmail.com>
2022-02-03 12:08:09 -0400

    Add display likes functionality

--- src/getDataFood.js
+++ src/getDataFood.js
@@ -1,15 +1,15 @@
 import getItemData from './createPopup.js';
 import hidenItems from './hidenItems.js';
-import { addLikeToItem } from './likeDataApi.js';
+import { addLikeToItem, getLikesToItems } from './likeDataApi.js';
 
 // function display an array of objects
 const displayData = (arr) => {
   const board = document.querySelector('.container-food-cards');
   board.innerHTML = '';
-
   arr.forEach((food) => {
     const item = document.createElement('article');
     item.id = food.idMeal;
+
     item.classList.add('card-food');
     item.innerHTML = `
         <div class="card-title">
@@ -18,7 +18,9 @@ const displayData = (arr) => {
         <button type="button" class="btn-recipe">Recipe</button>
         <div>        
         </div>
-          <button type="button" class="btn-liked">Like</button>
+        <div class="btn-liked">
+          Like
+        </div>
         <div class="card-img">
          <img src="${food.strMealThumb}" class="img-food">        
         </div>     
@@ -26,13 +28,30 @@ const displayData = (arr) => {
 
     const btnRecipeElement = item.querySelector('.btn-recipe');
     const btnLikedElement = item.querySelector('.btn-liked');
-    // event listener just for test
+    const printLike = (data) => {
+      const likesReturned = data.find(
+        // eslint-disable-next-line comma-dangle
+        (element) => element.item_id === food.idMeal
+      );
+      // eslint-disable-next-line operator-linebreak
+      btnLikedElement.innerHTML =
+        likesReturned !== undefined
+          ? `<i class="fas fa-heart"></i> (${likesReturned.likes})`
+          : '<i class="far fa-heart"></i> (0)';
+    };
+    getLikesToItems()
+      .then(printLike)
+      .catch((e) => console.log(e));
+
     btnRecipeElement.addEventListener('click', () => {
       getItemData(food.idMeal);
       hidenItems();
     });
     btnLikedElement.addEventListener('click', () => {
       addLikeToItem(food.idMeal);
+      getLikesToItems()
+        .then(printLike)
+        .catch((e) => console.log(e));
     });
     board.appendChild(item);
   });
